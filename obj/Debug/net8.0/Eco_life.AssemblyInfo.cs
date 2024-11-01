55b0889fc8e8a9741cd20aafb4594b285c57f75d
ANNIELE SILVA DOS SANTOS <021.898683@senaicimatec.edu.br>
2024-11-01 17:49:23 -0300

    first commit

--- obj/Debug/net8.0/Eco_life.AssemblyInfo.cs
+++ obj/Debug/net8.0/Eco_life.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eco_life")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3f540a10a9b8a661064af5ea2e01465f74026cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1a920623a2cc20e2197c6a8344a4ca7f44eafb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eco_life")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eco_life")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
